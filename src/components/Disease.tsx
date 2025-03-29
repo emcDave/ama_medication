e9b62b2e820a6001186d254da45b75f5f2e247d2
emcDave <sdaves3202@gmail.com>
2025-03-29 19:03:24 +0600

    second commit

--- src/components/Disease.tsx
+++ src/components/Disease.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import "./table.css"
 const Disease = () => {
   const diseases = [
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Column } from "./columns";
 import "./table.css";
 
